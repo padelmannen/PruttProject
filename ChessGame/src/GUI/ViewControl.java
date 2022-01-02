2c6cee2f24f97d98f3a3b733dee3631c99c97a69
fredrikstrandberg <fredrik.strandberg@live.se>
2022-01-02 11:09:02 +0100

    commit

---
+++ ChessGame/src/GUI/ViewControl.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import javax.swing.*;
+
+public class ViewControl extends JFrame {
+
+    public ViewControl(){
+        setFrame();
+
+        setVisible(true);
+    }
+
+    private void setFrame() {
+
+    }
+
+}
