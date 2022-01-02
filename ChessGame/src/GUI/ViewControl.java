542dba030b51039166c7a61748e2aa142c3e2b6b
fredrikstrandberg <fredrik.strandberg@live.se>
2022-01-02 11:52:28 +0100

    commit

--- ChessGame/src/GUI/ViewControl.java
+++ ChessGame/src/GUI/ViewControl.java
@@ -1,17 +1,24 @@
 package GUI;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class ViewControl extends JFrame {
+    private final String[] colors = {"Black","White"};
+    private final Integer[] rows = {1,2,3,4,5,6,7,8};
+    private final String[] columns = {"a","b","c","d","e","f","g","h"};
 
     public ViewControl(){
-        setFrame();
+        createBoard();
 
         setVisible(true);
     }
 
-    private void setFrame() {
+    private void createBoard() {
+            for(Integer row : rows) {
+                for (String column : columns) {
 
+            }
+        }
     }
-
 }
