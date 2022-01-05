de26038a878f2e33b5ba69e8da19331ba3df60d6
fredrikstrandberg <fredrik.strandberg@live.se>
2022-01-05 16:54:34 +0100

    viewcontrol

--- ChessGame/src/GUI/ViewControl.java
+++ ChessGame/src/GUI/ViewControl.java
@@ -1,4 +1,34 @@
 package GUI;
 
-public class ViewControl {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.*;
+
+class ViewControl extends JFrame implements ActionListener {
+
+    public ViewControl() throws IOException {
+        setupView();
+    }
+
+    public void setupView() {
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        new ViewControl();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
+
 }
